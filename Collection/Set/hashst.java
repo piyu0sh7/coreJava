5cd3320d6981ca3b666fa8c4378c82bfbf4bc012
Piyush Singh <piyush829104@gmail.com>
2025-02-04 16:24:04 +0530

    new

--- Collection/Set/hashst.java
+++ Collection/Set/hashst.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class hashst {
 public static void main(String[] args) {
     HashSet h1=new HashSet();
+    h1.add(50);
     h1.add(100);
     h1.add(50);
     h1.add(1);
---
+++ Collection/map/hmap.java
@@ -0,0 +1,22 @@
+package Collection.map;
+import java.util.*;
+public class hmap {
+    public static void main(String[] args) {
+        HashMap h1=new HashMap();
+        h1.put(01, "piyush");
+        h1.put("divyansh",1);
+        h1.put("Num",32);
+        h1.put(null,null);
+        System.out.println(h1);
+
+        LinkedHashMap h2=new LinkedHashMap();
+        h2.put(01, "piyush");
+        h2.put("divyansh",1);
+        h2.put("Num",32);
+        h2.put(null,null);
+        System.out.println(h2);
+    }
+    
+   
+    
+}
